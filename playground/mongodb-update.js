b3fbe14de5096c4fa0db2cd31f04fa04279a51cf
Abhishek Singhal <absing@sapient.com>
2017-04-17 22:29:53 +0530

    Add update script

---
+++ playground/mongodb-update.js
@@ -0,0 +1,44 @@
+const {MongoClient, ObjectID} = require('mongodb'); // syntax gets the MongoClient and ObjectID properties of mongoDB object
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (error, db)=>{
+    if(error){
+        console.log('Unable to connect to Mongodb server', error);
+        return;
+    }
+
+    
+    console.log('Connected to MongoDB server');
+    db.collection('Todos').findOneAndUpdate({
+        _id:new ObjectID('58f48a1ec30092ed3235372d')
+    },
+    {
+        $set : {
+            completed: true
+        }
+    }, 
+    {
+        returnOriginal: false
+    }).then((result) => {    
+        console.log('Result ', result);
+    });
+
+
+    db.collection('Users').findOneAndUpdate({
+        _id:new ObjectID('58f480de8c29120cece14efe')
+    },
+    {
+        $set : {
+            name: 'Abhishek'
+        },
+        $inc: {
+            age: 1
+        }
+    }, 
+    {
+        returnOriginal: false
+    }).then((result) => {    
+        console.log('Result ', result);
+    });
+
+    // db.close();
+});
