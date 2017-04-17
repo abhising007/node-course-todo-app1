e3e426e2ab9592945d711eaf1f1e5ad4a6fc12b0
Abhishek Singhal <absing@sapient.com>
2017-04-17 23:32:44 +0530

    added support for mongoose and post todo route

---
+++ server/server.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+var {mongoose}  = require('./db/mongoose.js');
+var {Todo} = require('./models/todo.js');
+var {User} = require('./models/user.js');
+
+var app = express();
+
+app.use(bodyParser.json());
+
+app.post('/todos', (req, res)=>{
+    var todo = new Todo({
+        text: req.body.text
+    });
+
+     todo.save().then((doc) => {
+         console.log('Saved todo', todo);
+         res.send(doc);
+     }, (e) => {
+         console.log('Unable to save todo');
+         res.status(400).send(e);
+     });
+    // console.log(req, res);
+});
+
+app.listen(3000, ()=>{
+    console.log('Server is up');
+});
