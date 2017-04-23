661a4fe68964bd366eb77cfe6647650304802896
Abhishek Singhal <absing@sapient.com>
2017-04-23 22:00:01 +0530

    Added mongoose playground query

---
+++ playground/mongoose-query.js
@@ -0,0 +1,40 @@
+const {ObjectId}=require('mongodb');
+
+const {mongoose} = require('./../server/db/mongoose');
+const {Todo} = require('./../server/models/todo');
+const {User} = require('./../server/models/user');
+
+var todo_id = '58fccecd807d5641a8a14cf0';
+
+if (!ObjectId.isValid(todo_id)){
+    console.log('Id not value');
+};
+
+Todo.find({
+    _id: todo_id
+}).then((todos) => {
+    console.log('todos:::: ', todos);
+});
+
+Todo.findOne({    
+    _id: todo_id}).then(todo => {
+        console.log('todo:::: ', todo);
+    });
+
+Todo.findById(todo_id).then(todo => {
+    if(!todo) {
+        return console.log('Id not found');
+    }
+    console.log('todoById:::: ', todo);
+}).catch((e)=> {
+    console.log('error',e);
+});
+
+User.findById('58fcd577e7ff7ae764b325fc').then((user) => {
+    if (!user){
+        return console.log('User id not found');
+    }
+    console.log('User id ::::', user);
+}).catch((e) => {
+    console.log('Error ', e);
+});
