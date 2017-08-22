4e3d6995a354605b951b54523f0c6bf02d1604ef
Abhishek Singhal <absing@sapient.com>
2017-08-22 22:23:10 +0530

    add post /users/login route

--- server/models/user.js
+++ server/models/user.js
@@ -70,6 +70,29 @@ UserSchema.statics.findByToken = function(token) {
     });
 };
 
+UserSchema.statics.findByCredentials = function(email, password) {
+    var User = this;
+
+    return User.findOne({'email': email}).then((user) => {
+        if (!user){
+            console.log("no user found");
+            return Promise.reject();
+        }
+
+        return new Promise((resolve, reject) =>{
+            bcrypt.compare(password, user.password, (err, res) => {
+                if (res){
+                    resolve(user);
+                } else {
+                    console.log("password not matching");
+                    reject();
+                }
+            });
+        });
+    });
+};
+
+
 UserSchema.pre('save', function(next){
     var user = this;
     if(user.isModified('password')){
--- server/server.js
+++ server/server.js
@@ -134,6 +134,22 @@ app.get('/users/me', authenticate, (req, res) => {
     res.send(req.user);
 });
 
+// login users
+app.post('/users/login', (req, res)=>{
+    // pick only properties that can be passed
+    var body = _.pick(req.body,['email','password']); 
+    
+     User.findByCredentials(body.email, body.password).then((user) => {
+        return user.generateAuthToken().then((token) => {
+            res.header('x-auth', token).send(user); 
+        });
+     }).catch((e) => {
+         console.log('Unable to find user');
+         res.status(400).send();
+     });
+});
+
+
 
 app.listen(port, ()=>{
     console.log(`Server is up at ${port}`);
